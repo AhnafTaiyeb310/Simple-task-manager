5419db84e80137996cdf3865633106e051ace9d3
AhnafTaiyeb310 <ahnaftaiyeb1@gmail.com>
2026-06-22 21:26:24 +0600

    first commit and backend setup

---
+++ backend/apps/tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . import models
+from .serializers import TaskSerializer
+
+
+class TaskModelViewSet(ModelViewSet):
+    queryset = models.Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
